3dc663952acce2cc7b3c5b9cad0b768378aca46c
Spencer Lombardo <spencerlombardo96@gmail.com>
2024-03-27 21:35:19 -0400

    changing form qml app to web app. Currently getting the backend server working. right now testing and working on getting the sqlx working to grab a single bound value

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,79 @@
-mod sqlreader;
-mod cxxqt_object;
-use cxx_qt_lib::{QGuiApplication, QQmlApplicationEngine, QUrl};
+use dotenv::dotenv;
+use anyhow::{anyhow, Context};
+use axum::{
+    extract::{Query, State}, http::StatusCode, routing::get, Router
+};
+use serde::Deserialize;
+use sqlx::MySqlPool;
 
-fn main() {
-    let mut app = QGuiApplication::new();
-    let mut engine = QQmlApplicationEngine::new();
-    
-    if let Some(engine) = engine.as_mut() {
-        engine.load(&QUrl::from("qrc:/qt/qml/demo/qml/main.qml"));
+#[derive(Deserialize)]
+pub struct DbQuery {
+    pub index: i32,
+}
+
+#[derive(sqlx::FromRow, Deserialize, Debug, Clone)]
+pub struct Lick {
+    id : i32,
+    filename : String,
+    learned : i32,
+}
+
+async fn index() -> &'static str {
+    return "Index";
+}
+
+async fn hello() -> &'static str {
+    return "Hello world"
+}
+
+async fn get_lick(pool: &MySqlPool, id: &i32) -> Result<Lick, anyhow::Error> {
+    let lick = sqlx::query_as::<_, Lick>(
+        "SELECT id,filename,learned FROM Licks where id=$1",)
+        .bind(id.to_string().as_str())
+        .fetch_optional(pool)
+        .await?;
+
+    println!("done");
+
+    if let Some(lick) = lick {
+        return Ok(lick);
     }
-
-    if let Some(app) = app.as_mut() {
-        app.exec();
+    else {
+        return Err(anyhow!("Entry not found: {id}"));
     }
 }
+
+async fn db_get(Query(params): Query<DbQuery>, State(pool): State<MySqlPool>) -> Result<String, StatusCode> {
+    let id: i32 = params.index;
+    match get_lick(&pool, &id).await {
+        Ok(lick) => Ok(format!(
+            "{}: {} {}", lick.id, lick.filename, lick.learned)),
+        Err(_) => Err(StatusCode::NO_CONTENT),
+    }
+
+}
+
+#[tokio::main]
+async fn main() ->anyhow::Result<()>{
+    // Use dotenv to load the .env file for the database url
+    dotenv().ok();
+    let db_connection_str = std::env::var("DATABASE_URL").context("DATABASE_URL must be set")?;
+    let pool = sqlx::MySqlPool::connect(&db_connection_str)
+        .await
+        .context("cant connect to database")?;
+
+    // Build our app with a single orute
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/hello", get(hello))
+        .route("/db", get(db_get))
+        .with_state(pool);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+
+} 
+
