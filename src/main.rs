2f1fbe1dd500063ffd2ccead2de141b1e7b7f544
Spencer Lombardo <spencerlombardo96@gmail.com>
2024-03-28 21:31:55 -0400

    got templates working, so it displays a "webpage"

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,59 @@
+use askama::Template;
 use dotenv::dotenv;
 use anyhow::{anyhow, Context};
 use axum::{
-    extract::{Query, State}, http::StatusCode, routing::get, Router
+    extract::{Query, State}, http::StatusCode, response::IntoResponse, routing::get, Router
 };
 use serde::Deserialize;
 use sqlx::MySqlPool;
 
+
+struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response();
+    }
+}
+
+impl<E> From<E> for AppError
+    where
+        E: Into<anyhow::Error>, {
+    fn from(err: E) -> Self {
+        return Self(err.into());
+    }
+}
+
+#[derive(Deserialize, Debug, Template)]
+#[template(path = "lick.html")]
+pub struct LickDisplay {
+    pub id : i32,
+    pub filename: String,
+    pub learned: i32 
+}
+
+impl LickDisplay { 
+    fn new(id: i32, filename: String, learned: i32) -> Self {
+        let ld = LickDisplay {
+            id,
+            filename,
+            learned
+        };
+        return ld;
+    }
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate;
+
 #[derive(Deserialize)]
 pub struct DbQuery {
-    pub index: i32,
+    pub id: i32,
 }
 
 #[derive(sqlx::FromRow, Deserialize, Debug, Clone)]
@@ -18,12 +63,8 @@ pub struct Lick {
     learned : i32,
 }
 
-async fn index() -> &'static str {
-    return "Index";
-}
-
-async fn hello() -> &'static str {
-    return "Hello world"
+async fn index() -> IndexTemplate{
+    return IndexTemplate;
 }
 
 async fn get_lick(pool: &MySqlPool, id: &i32) -> Result<Lick, anyhow::Error> {
@@ -41,13 +82,13 @@ async fn get_lick(pool: &MySqlPool, id: &i32) -> Result<Lick, anyhow::Error> {
     }
 }
 
-async fn db_get(Query(params): Query<DbQuery>, State(pool): State<MySqlPool>) -> Result<String, StatusCode> {
-    let id: i32 = params.index;
-    match get_lick(&pool, &id).await {
-        Ok(lick) => Ok(format!(
-            "{}: {} {}\n", lick.id, lick.filename, lick.learned)),
-        Err(_) => Err(StatusCode::NO_CONTENT),
-    }
+async fn db_get(Query(params): Query<DbQuery>, State(pool): State<MySqlPool>) -> Result<LickDisplay, AppError> {
+    let id: i32 = params.id;
+    let lick = get_lick(&pool, &id).await?;
+    return Ok(LickDisplay::new(
+        lick.id,
+        lick.filename, 
+        lick.learned));
 }
 
 #[tokio::main]
@@ -62,15 +103,11 @@ async fn main() ->anyhow::Result<()>{
     // Build our app with a single orute
     let app = Router::new()
         .route("/", get(index))
-        .route("/hello", get(hello))
         .route("/db", get(db_get))
         .with_state(pool);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
-    axum::serve(listener, app).await.unwrap();
-
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
     Ok(())
-
 } 
 
