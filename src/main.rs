e6f81ab38c36bb23ebc587dcc542bdffecf3b3de
Spencer Lombardo <spencerlombardo96@gmail.com>
2024-03-29 23:02:15 -0400

    IT WORKS. figured out the post request

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use askama::Template;
+use askama_axum::Template;
 use dotenv::dotenv;
 use anyhow::{anyhow, Context};
 use axum::{
-    extract::{Query, State}, http::StatusCode, response::IntoResponse, routing::get, Router
+    extract::{Query, State}, http::StatusCode, response::IntoResponse, routing::{get, post}, Form, Router
 };
 use serde::Deserialize;
 use sqlx::MySqlPool;
-
 
 struct AppError(anyhow::Error);
 
@@ -47,6 +46,21 @@ impl LickDisplay {
     }
 }
 
+#[derive(Deserialize, Debug, Template)]
+#[template(path = "licks.html")]
+pub struct LicksDisplay {
+    pub licks: Vec<Lick>,
+}
+
+impl LicksDisplay{
+    fn new(licks: Vec<Lick>) -> Self {
+        let ld = LicksDisplay {
+            licks
+        };
+        return ld;
+    }
+}
+
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate;
@@ -54,6 +68,11 @@ struct IndexTemplate;
 #[derive(Deserialize)]
 pub struct DbQuery {
     pub id: i32,
+}
+
+#[derive(Deserialize)]
+pub struct DbAdd {
+    pub file: String,
 }
 
 #[derive(sqlx::FromRow, Deserialize, Debug, Clone)]
@@ -91,6 +110,39 @@ async fn db_get(Query(params): Query<DbQuery>, State(pool): State<MySqlPool>) ->
         lick.learned));
 }
 
+async fn add_lick_db(pool: &MySqlPool, filename: &String) -> Result<i32, anyhow::Error> {
+    let tmp = format!("INSERT INTO Licks (filename) values ('{}')", filename);
+    sqlx::query(&tmp).execute(pool).await?;
+    return Ok(0);
+}
+
+async fn add_lick(State(pool): State<MySqlPool>, Form(params): Form<DbAdd>) -> IndexTemplate{
+    let filename = params.file;
+    if filename.eq("") {
+            return IndexTemplate;
+    }
+
+    match add_lick_db(&pool, &filename).await {
+        Ok(_) => {
+            return IndexTemplate;
+        }
+        Err(_) => {
+            return IndexTemplate;
+        }
+    }
+}
+
+async fn list_licks(State(pool): State<MySqlPool>) -> Result<LicksDisplay, AppError> {
+    let licks = get_licks(&pool).await?;
+    return Ok(LicksDisplay::new(licks));
+}
+
+async fn get_licks(pool: &MySqlPool) -> Result<Vec<Lick>, anyhow::Error> {
+    let tmp: String = format!("SELECT id,filename,learned FROM Licks");
+    let licks = sqlx::query_as(&tmp).fetch_all(pool).await?;
+    Ok(licks)
+}
+
 #[tokio::main]
 async fn main() ->anyhow::Result<()>{
     // Use dotenv to load the .env file for the database url
@@ -104,10 +156,11 @@ async fn main() ->anyhow::Result<()>{
     let app = Router::new()
         .route("/", get(index))
         .route("/db", get(db_get))
+        .route("/licks", get(list_licks))
+        .route("/add_lick", post(add_lick))
         .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(listener, app).await?;
     Ok(())
 } 
-
