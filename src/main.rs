e92b6274795b25f1596dd149eb1cdc29c96ac0e5
Spencer Lombardo <spencerlombardo96@gmail.com>
2024-03-30 20:31:24 -0400

    file upload works. need to get file showing on html

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use std::fs;
+
 use askama_axum::Template;
 use dotenv::dotenv;
 use anyhow::Context;
 use axum::{
-    extract::State, http::StatusCode, response::IntoResponse, routing::{get, post}, Form, Router
+    extract::Multipart,
+    extract::State, 
+    http::StatusCode, 
+    response::IntoResponse, 
+    routing::get, 
+    Router
 };
 use serde::Deserialize;
 use sqlx::MySqlPool;
@@ -42,6 +49,22 @@ impl LicksDisplay{
     }
 }
 
+
+#[derive(Deserialize, Debug, Template)]
+#[template(path = "lick_pdf.html")]
+pub struct PdfDisplay {
+    pub pdf: String,
+}
+
+impl PdfDisplay{
+    fn new(filename: String) -> Self {
+        let ld = PdfDisplay {
+            pdf: filename
+        };
+        return ld;
+    }
+}
+
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate;
@@ -67,26 +90,10 @@ async fn index() -> IndexTemplate{
     return IndexTemplate;
 }
 
-async fn add_lick_db(pool: &MySqlPool, filename: &String) -> Result<i32, anyhow::Error> {
-    let tmp = format!("INSERT INTO Licks (filename) values ('{}')", filename);
-    sqlx::query(&tmp).execute(pool).await?;
-    return Ok(0);
-}
-
-async fn add_lick(State(pool): State<MySqlPool>, Form(params): Form<DbAdd>) -> IndexTemplate{
-    let filename = params.file;
-    if filename.eq("") {
-            return IndexTemplate;
-    }
-
-    match add_lick_db(&pool, &filename).await {
-        Ok(_) => {
-            return IndexTemplate;
-        }
-        Err(_) => {
-            return IndexTemplate;
-        }
-    }
+async fn get_licks(pool: &MySqlPool) -> Result<Vec<Lick>, anyhow::Error> {
+    let tmp: String = format!("SELECT id,filename,learned FROM Licks");
+    let licks = sqlx::query_as(&tmp).fetch_all(pool).await?;
+    Ok(licks)
 }
 
 async fn list_licks(State(pool): State<MySqlPool>) -> Result<LicksDisplay, AppError> {
@@ -94,10 +101,40 @@ async fn list_licks(State(pool): State<MySqlPool>) -> Result<LicksDisplay, AppEr
     return Ok(LicksDisplay::new(licks));
 }
 
-async fn get_licks(pool: &MySqlPool) -> Result<Vec<Lick>, anyhow::Error> {
-    let tmp: String = format!("SELECT id,filename,learned FROM Licks");
-    let licks = sqlx::query_as(&tmp).fetch_all(pool).await?;
-    Ok(licks)
+async fn grab_file(pool: &MySqlPool) -> Result<String, anyhow::Error> {
+    let tmp: String = format!("SELECT id,filename,learned from Licks LIMIT 1");
+    let lick: Lick = sqlx::query_as(&tmp).fetch_one(pool).await?;
+    return Ok(lick.filename);
+}
+
+async fn serve_pdf(State(pool): State<MySqlPool>) -> Result<PdfDisplay, AppError> {
+    let filename = grab_file(&pool).await?;
+    return Ok(PdfDisplay::new(filename));
+}
+
+async fn add_lick_db(pool: &MySqlPool, filename: &String) -> Result<i32, anyhow::Error> {
+    let tmp = format!("INSERT INTO Licks (filename) values ('{}')", filename);
+    sqlx::query(&tmp).execute(pool).await?;
+    return Ok(0);
+}
+
+async fn upload_lick_pdf(State(pool):State<MySqlPool>, mut multipart: Multipart) -> IndexTemplate{
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        let ctype = field.content_type().unwrap().to_string();
+        let data = field.bytes().await.unwrap();
+        println!("Length of '{}' is {} bytes; type {}", 
+            name, 
+            data.len(),
+            ctype);
+        if ctype.contains("pdf") {
+            let filename = format!("./data/{}.pdf",name);
+            let _ = fs::write(&filename, data);
+            let _ = add_lick_db(&pool, &filename).await;
+        }
+    }
+
+    return IndexTemplate;
 }
 
 #[tokio::main]
@@ -111,9 +148,9 @@ async fn main() ->anyhow::Result<()>{
 
     // Build our app with a single orute
     let app = Router::new()
-        .route("/", get(index))
+        .route("/", get(index).post(upload_lick_pdf))
         .route("/licks", get(list_licks))
-        .route("/add_lick", post(add_lick))
+        .route("/pdf", get(serve_pdf))
         .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
