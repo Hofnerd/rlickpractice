5f0b093b1926730e0b8fced6132a26eefcba0623
Spencer Lombardo <spencerlombardo96@gmail.com>
2024-04-03 20:00:53 -0400

    working on learning tailwind css for the system. add form looks pretty good table also looks better as well.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,29 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./templates/*.html'],
-  theme: {
-    extend: {
+    content: ['./templates/*.html'],
+    theme: {
+        extend: {
             fontFamily: {
                 sans: ['Inter var', ...fontFamily.sans],
+            },
+            colors: {
+                'sail': {
+                     '50' : '#f2f9fd',
+                    '100' : '#e4f1fa',
+                    '200' : '#c4e4f4',
+                    '300' : '#8eceeb',
+                    '400' : '#52b4de',
+                    '500' : '#2c9ccb',
+                    '600' : '#1d7cac',
+                    '700' : '#19648b',
+                    '800' : '#185574',
+                    '900' : '#194761',
+                    '950' : '#112d40', 
+                },
+            },
         },
     },
-  },
     plugins: [require('@tailwindcss/forms')],
 }
 
